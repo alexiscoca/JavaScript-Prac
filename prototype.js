0fee27d49075754b72710a6adf61334e1d5f5afd
alexiscoca <alexis.coca@ciencias.unam.mx>
2022-09-05 15:51:55 -0500

    Prototype

--- prototype.js
+++ prototype.js
@@ -7,11 +7,38 @@ function Person(name, lastName) {     //Parametros a la funcion
 }
 
 const alexis = new Person("Alexis", "Coca")     //Aquí le pasamos un parametro en forma de String      
-console.log(alexis.displayName())                 //Esto imprime el nombre completo "Alexis Coca"
+alexis.name = "Alexis"
+                                                 //Esto imprime el nombre completo "Alexis Coca"
 
-const memo = new Person("Memo", "Caballero")                    //El primer parametro es una propiedad de name y el 2do de lastName
-console.log(memo.displayName())                       //Esto imprime el nombre y apellido concatenados
+const memo = new Person("Memo", "Caballero")                //El primer parametro es una propiedad de name y el 2do de lastName
+                                                           //Esto imprime el nombre y apellido concatenados
+
+const comal = new Person("Comal", "Ezquivel")
+const keps = new Person("Keps", "Herre")
 
 
+//agregando un metodo al objeto Person con PROTOTYPE
+Person.prototype.greet = function() {
+    return "Hello I'am " + this.name
+}
+Person.prototype.age = function() {         //Con el Person.prototype estamos añadiendo un metodo al Objeto Person, no al constructor   
+    return "and i am" + " " + 21
+}
+console.log(alexis.greet())
+console.log(memo.greet())
+console.log(keps.greet(), keps.age())
+console.log(comal.greet())
 
-//john es instancia de Person"
+//otro ejemplo
+const s = new String("Hola mundo")
+String.prototype.concatTest = function() {
+    return this + " " + "test"
+}
+console.log(s.concatTest())
+console.log("My name is Alexis".concatTest())
+
+//alexis es instancia de Person"
+//puedo agregar metodos en un constructor ya creado
+/*prototype hace referencia a la estructura que ya tenía un constructor y 
+ todos los objetos que deciendan de el estan conectados con el constructor para poder acceder a sus propiedades
+  y metodos */
